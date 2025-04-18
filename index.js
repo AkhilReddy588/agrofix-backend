c02906e76ea5c96a9ee3a965e56c4d128da260bb
Nerati Akhil Reddy <akhil990836@gmail.com>
2025-04-18 22:41:59 +0530

    cors change

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(cors({
+  origin: ['http://localhost:3001', 'http://localhost:3002'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 dbConnect()
 
 app.use('/api/users', authRoute)
