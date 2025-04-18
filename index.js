0a0a09b8d6dfca99bb84c3e877337cd0fbde49ab
Nerati Akhil Reddy <akhil990836@gmail.com>
2025-04-18 23:11:12 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const serverless = require('serverless-http');
 
 const app = express()
 app.use(express.json())
-app.use(cors())
 
 app.use(cors({
   origin: ['http://localhost:3001', 'http://localhost:3002'],
