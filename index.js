8ff0491e77bdee8bbde5eee427301d8d617ad3fd
Nerati Akhil Reddy <akhil990836@gmail.com>
2025-04-18 23:36:16 +0530

    cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-  origin: ['http://localhost:3001', 'http://localhost:3002', 'https://agrofix-user-frontend.vercel.app'],
+  origin: ['http://localhost:3001', 'http://localhost:3002', 'https://agrofix-user-frontend.vercel.app', 'https://agrofix-admin-ebon.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
